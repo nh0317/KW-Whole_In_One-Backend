a8a4e973e430a8aa1d078ee095733b479f241eef
KNH <nh2004@naver.com>
2022-01-07 19:24:37 +0900

    Fix : 테스트 코드 삭제

--- src/main/java/com/naturemobility/seoul/BackendApplication.java
+++ src/main/java/com/naturemobility/seoul/BackendApplication.java
@@ -8,15 +8,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-@RestController
 @EnableDiscoveryClient
 public class BackendApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(BackendApplication.class, args);
-	}
-	@GetMapping("/config")
-	public String string(@Value("${jwt.secret.user_info_password_key}") String key){
-		return key;
 	}
 }
