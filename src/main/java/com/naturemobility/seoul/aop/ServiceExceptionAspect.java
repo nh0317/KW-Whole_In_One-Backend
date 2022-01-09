5166232aa9c0054b073ec7a7ee0dc4fdedfec0f2
KNH <nh2004@naver.com>
2022-01-09 16:22:58 +0900

    Add : aop 설정 추가
    aop로 예외처리 로직 구현

---
+++ src/main/java/com/naturemobility/seoul/aop/ServiceExceptionAspect.java
@@ -0,0 +1,24 @@
+package com.naturemobility.seoul.aop;
+
+import com.naturemobility.seoul.config.BaseException;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.stereotype.Component;
+
+@Component
+@Aspect
+public class ServiceExceptionAspect {
+    //TODO:utils 의 함수도 포함
+    @Around("execution(* com.naturemobility.seoul.service.*.*(..))")
+    public Object serviceExceptionHandler(ProceedingJoinPoint proceedingJoinPoint) throws BaseException {
+        try{
+            return proceedingJoinPoint.proceed();
+        }catch (BaseException e){
+            throw new BaseException(e.getStatus());
+        }catch (Throwable e){
+            throw new RuntimeException(e.getMessage());
+        }
+    }
+
+}
