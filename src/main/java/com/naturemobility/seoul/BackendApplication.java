367037ff92541694613209305e9850e63029f247
KNH <nh2004@naver.com>
2022-01-14 17:45:04 +0900

    Add : timezone 설정

--- src/main/java/com/naturemobility/seoul/BackendApplication.java
+++ src/main/java/com/naturemobility/seoul/BackendApplication.java
@@ -7,9 +7,16 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.annotation.PostConstruct;
+import java.util.TimeZone;
+
 @SpringBootApplication
 @EnableDiscoveryClient
 public class BackendApplication {
+	@PostConstruct
+	void started() {
+		TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(BackendApplication.class, args);
 	}
