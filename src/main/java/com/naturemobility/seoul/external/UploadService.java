d24d3668fa7643f8e609d953f125606d8ff87145
liyusang1 <liyusang1@naver.com>
2022-02-16 01:58:15 +0900

    Update : FileUploadController 추가

---
+++ src/main/java/com/naturemobility/seoul/external/UploadService.java
@@ -0,0 +1,13 @@
+package com.naturemobility.seoul.config;
+
+import com.amazonaws.services.s3.model.ObjectMetadata;
+
+import java.io.InputStream;
+
+public interface UploadService {
+
+    void uploadFile(InputStream inputStream, ObjectMetadata objectMetadata, String fileName);
+
+    String getFileUrl(String fileName);
+
+}
