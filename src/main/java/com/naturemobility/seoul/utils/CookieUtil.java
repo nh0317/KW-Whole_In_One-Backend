3588015dc9ad3cddf458c90975f7acf63bd3bf81
KNH <nh2004@naver.com>
2022-05-03 16:27:59 +0900

    fix : httpOnly 설정 변경

--- src/main/java/com/naturemobility/seoul/utils/CookieUtil.java
+++ src/main/java/com/naturemobility/seoul/utils/CookieUtil.java
@@ -11,7 +11,7 @@ import javax.servlet.http.HttpServletResponse;
 public class CookieUtil {
     public Cookie createCookie(String name, String value, int validity){
         Cookie token = new Cookie(name, value);
-        token.setHttpOnly(false);
+        token.setHttpOnly(true);
         token.setMaxAge(validity);
         token.setPath("/");
         return token;
