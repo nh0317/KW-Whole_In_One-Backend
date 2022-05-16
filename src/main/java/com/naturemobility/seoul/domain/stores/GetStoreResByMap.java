4db65992438587e30a3a2cae1cf210ceb2575423
liyusang1 <liyusang1@naver.com>
2022-05-16 20:56:23 +0900

    Fix : 지도 api reserveStatus,couponStatus 정보 추가

--- src/main/java/com/naturemobility/seoul/domain/stores/GetStoreResByMap.java
+++ src/main/java/com/naturemobility/seoul/domain/stores/GetStoreResByMap.java
@@ -11,6 +11,8 @@ import lombok.Setter;
 @NoArgsConstructor
 public class GetStoreResByMap {
     private Long storeIdx;
+    private Integer reserveStatus;
+    private Integer couponStatus;
     private String storeImage="https://i.ibb.co/h1kC5wc/image.png";
     private String storeBrand;
     private String storeName;
